0aaf917c4417154665ab3c6c9d08b5bc05a156db
Samuel Torres <torresromerosamuelandres@gmail.com>
2025-03-30 14:25:41 -0500

    proyecto peliculas erick

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -78,7 +78,6 @@ const App = () => {
   useEffect(() => {
     fetchMovies(debouncedSearchTerm);
   }, [debouncedSearchTerm]);
-
   useEffect(() => {
     loadTrendingMovies();
   }, []);
@@ -90,14 +89,14 @@ const App = () => {
       <div className="wrapper">
         <header>
           <img src="./hero.png" alt="Hero Banner" />
-          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+          <h1>Encuentra <span className="text-gradient">peliculas </span> que disfrutarás sin complicaciones</h1>
 
-          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+           <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
         {trendingMovies.length > 0 && (
           <section className="trending">
-            <h2>Trending Movies</h2>
+            <h2>Peliculas tendencia</h2>
 
             <ul>
               {trendingMovies.map((movie, index) => (
@@ -111,7 +110,7 @@ const App = () => {
         )}
 
         <section className="all-movies">
-          <h2>All Movies</h2>
+          <h2>Todas las peliculas</h2>
 
           {isLoading ? (
             <Spinner />
